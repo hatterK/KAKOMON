81cafd84a32c871ba9b65bf94df550854574ea7a
bo40 <bo40.git@gmail.com>
2016-08-20 23:42:34 +0000

    memberモデルにバリデーションとユーザー認証の機能を追加した。
    migrateファイルにデータのカラムを追加し、memberモデルには、
    バリデーションとユーザー認証を行うメソッドを定義した。
    memberモデルはハッシュされたパスワードを持つようにし、
    そのために、gemfileにbcryptを追加した。

--- Kakomon/app/models/member.rb
+++ Kakomon/app/models/member.rb
@@ -1,2 +1,31 @@
 class Member < ActiveRecord::Base
+
+  validates :name, presence: true,
+    format: { with: /\A[A-Za-z]\w*\z/, allow_blank: true },
+    length: { minimum: 2, maximum: 20, allow_blank: true },
+    uniqueness: { case_sensitive: false }
+  validates :password, presence: { on: create },
+    confirmation: { allow_blank: true }
+  validates :access_authority, presence: true,
+    numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 3 }
+
+
+  attr_accessor :password, :password_confirmation
+
+  def password=(val)
+    if val.present?
+      self.hashed_password = BCrypt::Password.create(val)
+    end
+    @password = val
+  end
+
+  def authenticate( name, password )
+    member = find_by(name: name)
+    if member && member.hashed_password.present? && BCrypt::Password.new(member.hashed_password) == password
+      member
+    else
+      nil
+    end
+  end
+
 end
