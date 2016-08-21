027249164cc17b735a7806d3d3cfb35dd76b7421
bo40 <bo40.git@gmail.com>
2016-08-21 10:28:09 +0000

    members_controllerを作成して、アクションとビューを簡単に実装した
    memberモデルを扱うmembers_controllerを作成して、7つのアクションと
    それに対応する単純なviewを追加した。

---
+++ Kakomon/app/controllers/members_controller.rb
@@ -0,0 +1,48 @@
+class MembersController < ApplicationController
+  def index
+    @members = Member.order(:id)
+  end
+
+  def show
+    @member = Member.find(params[:id])
+  end
+
+  def new
+    @member = Member.new
+  end
+
+  def edit
+    @member = Member.find(params[:id])
+  end
+
+  def create
+    @member = Member.new(member_params)
+    if @member.save
+      redirect_to @member, notice: "メンバーを追加しました。"
+    else
+      render "new"
+    end
+  end
+
+  def update
+    @member = Member.find(params[:id])
+    @member.assign_attributes(member_params)
+    if @member.save
+      redirect_to @member, notice: "メンバーの情報を更新しました。"
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @member = Member.find(params[:id])
+    @member.destroy
+    redirect_to :members, notice: "メンバーを削除しました。"
+  end
+
+  def member_params
+    attrs = [:name, :password, :password_confirmation]
+    attrs << :access_authority
+    params.require(:member).permit(attrs)
+  end
+end
