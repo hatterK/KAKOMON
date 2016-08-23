8eb925e7d997517a14760ecec7a83a3588aa1dde
Yusuke Sangenya <longinus.eva@gmail.com>
2016-08-23 14:01:56 +0900

    exam_dateのテストを書いた
    まだviewがなかったのでとりあえずは実行を保留するように

--- Kakomon/spec/requests/exam_dates_spec.rb
+++ Kakomon/spec/requests/exam_dates_spec.rb
@@ -1,10 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe "ExamDates", type: :request do
-  describe "GET /exam_dates" do
-    it "works! (now write some real specs)" do
-      get exam_dates_index_path
-      expect(response).to have_http_status(200)
+  describe 'ステータスコードチェック' do
+    let!(:exam_date) { ExamDate.create(year: rand(2010..2015), term: ['前期', '後期'].sample) }
+    before do
+      login Member.create(name: 'some_general_member', access_authority: 2, password: 'some_password')
+    end
+
+    xit 'index' do
+      get exam_dates_path
+
+      expect(response).to have_http_status(:ok)
+    end
+
+    xit 'show' do
+      get (exam_date_path id: exam_date.id)
+
+      expect(response).to have_http_status(:ok)
+    end
+
+    xit 'search_by_year' do
+      get search_by_year_exam_dates_path, { year: exam_date.year }
+
+      expect(response).to have_http_status(:ok)
+    end
+
+    xit 'search_by_term' do
+      get search_by_term_exam_dates_path, { term: exam_date.term }
+
+      expect(response).to have_http_status(:ok)
     end
   end
 end
