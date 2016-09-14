b03a62de8f62086781a7c13073671b6829a5fccf
bo40 <bo40.git@gmail.com>
2016-09-14 10:08:10 +0900

    ユーザー名とパスワードが間違っていた時のリダイレクトを修正
    より適切なページに移動するようにした。

--- Kakomon/app/controllers/sessions_controller.rb
+++ Kakomon/app/controllers/sessions_controller.rb
@@ -3,10 +3,11 @@ class SessionsController < ApplicationController
     member = Member.authenticate(params[:name], params[:password])
     if member
       session[:member_id] = member.id
+      redirect_to :root
     else
       flash.alert = 'ユーザー名とパスワードが一致しません。'
+      render "members/index"
     end
-    redirect_to :root
   end
 
   def destroy
