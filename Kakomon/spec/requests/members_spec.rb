0206a39ba910e572c1e3718a32968a238704b899
bo40 <bo40.git@gmail.com>
2016-09-22 06:12:37 +0900

    members_controllerのテストを修正した。

--- Kakomon/spec/requests/members_spec.rb
+++ Kakomon/spec/requests/members_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "Members", type: :request do
   describe 'ステータスコードチェック' do
-    let(:super_member)   { Member.create(name: 'some_super_member', access_authority: 1, password: 'some_password')}
-    let(:general_member) { Member.create(name: 'some_general_member', access_authority: 2, password: 'some_password') }
+    let(:admin_member)   { Member.create(name: 'some_admin_member', access_authority: 1, password: 'some_password')}
+    let(:user_member) { Member.create(name: 'some_user_member', access_authority: 3, password: 'some_password') }
 
     it 'index' do
       get members_path
@@ -11,43 +11,43 @@ RSpec.describe "Members", type: :request do
     end
 
     it 'new' do
-      login super_member
-      get new_member_path
+      login admin_member
+      get new_admin_member_path
 
       expect(response).to have_http_status(:ok)
     end
 
     it 'edit' do
-      login super_member
-      get (edit_member_path id: general_member.id)
+      login admin_member
+      get (edit_admin_member_path id: user_member.id)
 
       expect(response).to have_http_status(:ok)
     end
 
     it 'show' do
-      login super_member
-      get (member_path id: general_member)
+      login admin_member
+      get (admin_member_path id: user_member.id)
 
       expect(response).to have_http_status(:ok)
     end
 
     it 'create' do
-      login super_member
-      post members_path, member: { name: 'some_new_memeber', password: 'new_password', password_confirmation: 'new_password', access_authority: 1 }
+      login admin_member
+      post admin_members_path, member: { name: 'some_new_memeber', password: 'new_password', password_confirmation: 'new_password', access_authority: 1 }
 
       expect(response).to have_http_status(:see_other)
     end
 
     it 'update' do
-      login super_member
-      put (member_path id: general_member.id), member: { name: 'some_changed_name' }
+      login admin_member
+      put (admin_member_path id: user_member.id), member: { name: 'some_changed_name' }
 
       expect(response).to have_http_status(:see_other)
     end
 
     it 'destroy' do
-      login super_member
-      delete (member_path id: general_member.id)
+      login admin_member
+      delete (admin_member_path id: user_member.id)
 
       expect(response).to have_http_status(:see_other)
     end
