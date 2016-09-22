eef733dd30ff3c6480aed77c2be527cd93522702
bo40 <bo40.git@gmail.com>
2016-09-22 09:09:46 +0900

    exam_dates_controllerのテストを実装した。

--- Kakomon/spec/requests/exam_dates_spec.rb
+++ Kakomon/spec/requests/exam_dates_spec.rb
@@ -2,31 +2,55 @@ require 'rails_helper'
 
 RSpec.describe "ExamDates", type: :request do
   describe 'ステータスコードチェック' do
-    let!(:exam_date) { ExamDate.create(year: rand(2011..2015), term: ['前期', '後期'].sample) }
+    let!(:exam_date) { ExamDate.create(year: rand(2011..2015), term: ['first_semester', 'second_semester'].sample) }
     before do
-      login Member.create(name: 'some_general_member', access_authority: 2, password: 'some_password')
+      login Member.create(name: 'some_admin_member', access_authority: 1, password: 'some_password')
     end
 
-    xit 'index' do
-      get exam_dates_path
+    it 'index' do
+      get admin_exam_dates_path
 
       expect(response).to have_http_status(:ok)
     end
 
-    xit 'show' do
-      get (exam_date_path id: exam_date.id)
+    it 'show' do
+      get (admin_exam_date_path id: exam_date.id)
 
       expect(response).to have_http_status(:ok)
     end
 
-    xit 'search_by_year' do
-      get search_by_year_exam_dates_path, { year: exam_date.year }
+    it 'new' do
+      get new_admin_exam_date_path
 
       expect(response).to have_http_status(:ok)
     end
 
-    xit 'search_by_term' do
-      get search_by_term_exam_dates_path, { term: exam_date.term }
+    it 'edit' do
+      get (edit_admin_exam_date_path id: exam_date.id)
+
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'create' do
+      post admin_exam_dates_path, exam_date: exam_date.attributes.symbolize_keys.except(:id, :created_at, :updated_at)
+
+      expect(response).to have_http_status(:see_other)
+    end
+
+    it 'update' do
+      put (admin_exam_date_path id: exam_date.id), exam_date: { year: '2025' }
+
+      expect(response).to have_http_status(:see_other)
+    end
+
+    it 'destroy' do
+      delete (admin_exam_date_path id: exam_date.id)
+
+      expect(response).to have_http_status(:see_other)
+    end
+
+    it 'search' do
+      get search_admin_exam_dates_path, { search_year: exam_date.year, search_term: exam_date.term }
 
       expect(response).to have_http_status(:ok)
     end
