d43607b784794098b63c8c1a7c0b6b0ba09b741e
bo40 <bo40.git@gmail.com>
2016-09-26 07:12:26 +0900

    objectタグにbase64で画像データを埋め込んで表示させるように修正した。
    これにより認証しないと画像データにアクセスすることが出来なくなった。

--- Kakomon/app/helpers/past_questions_helper.rb
+++ Kakomon/app/helpers/past_questions_helper.rb
@@ -1,7 +1,15 @@
 module PastQuestionsHelper
-  def image_path
-    path1 = @past_question.image.current_path
-    path1.slice!("#{Rails.root.join("app/assets/images")}/")
-    path1
+  def base64_image_data
+    # path1 = @past_question.image.current_path
+    # path1.slice!("#{Rails.root.join("app/assets/images")}/")
+    # path1
+    ext = File.extname(@past_question.image.current_path)
+    if(ext == '.pdf')
+      'data:application/pdf;base64,' + Base64.strict_encode64(File.binread(@past_question.image.current_path))
+    elsif(ext == '.jpg' || ext == '.jpeg')
+      'data:image/jpg;base64,' + Base64.strict_encode64(File.binread(@past_question.image.current_path))
+    elsif(ext == 'png')
+      'data:image/png;base64,' + Base64.strict_encode64(File.binread(@past_question.image.current_path))
+    end
   end
 end
