842ee5ab71c9463a1220544b0dffadc5ca551d61
bo40 <bo40.git@gmail.com>
2016-10-07 13:25:43 +0900

    tagの名前が10文字は少ないので20文字までに変更した。

--- Kakomon/app/models/tag.rb
+++ Kakomon/app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ActiveRecord::Base
   validates :name, presence: true
-  validates :name, length: { maximum: 10 }
+  validates :name, length: { maximum: 20 }
 
   has_many :tag_relations, dependent: :destroy
   has_many :past_questions, through: :tag_relations
